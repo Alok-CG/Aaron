4c4b1b10122868a787042e5f9a6b20d3aa4b1658
Alok Kumar <alok.kumar@capgemini.com>
2021-07-27 16:13:41 +0530

    Commiting the Automation project from Bash Area

---
+++ Automation/src/test/java/QAAutomation/Automation/AppTest.java
@@ -0,0 +1,13 @@
+package QAAutomation.Automation;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    public static void main(String args){
+    	System.out.println("I am in AppTest");
+    }
+}
---
+++ Automation/src/test/java/QAAutomation/Automation/AppTest1.java
@@ -0,0 +1,13 @@
+package QAAutomation.Automation;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest1 extends TestCase
+{
+    public static void main(String args){
+    	System.out.println("I am in AppTest1");
+    }
+}
